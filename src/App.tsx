52ea33531df169f3ff01f65e3e305d9b6c68703f
LimeIvy <raimu10311221@yahoo.co.jp>
2025-11-04 23:02:30 +0900

    prettierで自動フォーマット

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 const App = () => {
   return (
@@ -8,14 +8,14 @@ const App = () => {
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
